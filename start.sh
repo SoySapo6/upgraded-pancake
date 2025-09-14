590e00a5cc6d0524a147fb2cf55a05b13c813235
𝐒𝐨𝐲𝕄𝕒𝕪𝕔𝕠𝕝 ᵒᶠⁱᶜⁱᵃˡ <karatekidamericatv@gmail.com>
2025-09-14 14:15:54 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-./java8/bin/java -Xmx256M -Xms128M -XX:+UseSerialGC -XX:+DisableExplicitGC -XX:-UseGCOverheadLimit -jar paper.jar --port "$SERVER_PORT" nogui
+# Detectar carpeta de Java disponible
+if [ -d "java21" ]; then
+    JAVA_PATH="./java21/bin/java"
+elif [ -d "java17" ]; then
+    JAVA_PATH="./java17/bin/java"
+elif [ -d "java11" ]; then
+    JAVA_PATH="./java11/bin/java"
+elif [ -d "java8" ]; then
+    JAVA_PATH="./java8/bin/java"
+else
+    JAVA_PATH="java"
+fi
+
+$JAVA_PATH -Xmx256M -Xms128M -XX:+UseSerialGC -XX:+DisableExplicitGC -XX:-UseGCOverheadLimit -jar paper.jar --port "$SERVER_PORT" nogui
